6716504171a76324e5ea5c6b48c2b3bf48e80d3f
Aayush Patel <mraayush@gmail.com>
2026-04-04 23:43:15 -0400

    fix: robocopy XF for obsidian workspace files, wildcards not valid in XD

--- _agent/scripts/drive_mirror.bat
+++ _agent/scripts/drive_mirror.bat
@@ -1,5 +1,5 @@
 @echo off
 set SOURCE=C:\Data\Code\Market Research
 set DEST=G:\My Drive\Market Research Mirror
-robocopy "%SOURCE%" "%DEST%" /MIR /XD ".git" "_agent\logs" "_agent\slack_adapter\.venv" ".obsidian\workspace*" /XF ".env" "*.tmp" /R:2 /W:5 /NFL /NDL /NP /LOG+:"C:\Data\Code\Market Research\_agent\logs\mirror.log"
+robocopy "%SOURCE%" "%DEST%" /MIR /XD ".git" "_agent\logs" "_agent\slack_adapter\.venv" /XF ".env" "*.tmp" "workspace.json" "workspace-mobile.json" "workspaces.json" /R:2 /W:5 /NFL /NDL /NP /LOG+:"C:\Data\Code\Market Research\_agent\logs\mirror.log"
 exit /b 0
